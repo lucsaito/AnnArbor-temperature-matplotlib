e992b5ecf75e685caab7e2d8e53a021157a78a67
Lucas <lukinhassaito2013@hotmail.com>
2020-07-14 20:23:09 -0300

    Creating the second plot

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ data = pd.read_csv("fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89.csv
 data_max = data[data['Element'] == "TMAX"].groupby('Date').max()
 data_min = data[data['Element'] == "TMIN"].groupby('Date').min()
 
-
-data_max = data_max['2005-01-01':'2014-12-31']
-data_min = data_min['2005-01-01':'2014-12-31']
-
-
 data_max['Data_Value'] = data_max['Data_Value']/10
 data_min['Data_Value'] = data_min['Data_Value']/10
 
@@ -23,27 +18,73 @@ for i in range(2005, 2015):
     except:
         pass
 
-data_range_max = list(map(pd.to_datetime, data_max.index))
-data_range_min = list(map(pd.to_datetime, data_min.index))
 
-plt.plot(data_range_max, data_max['Data_Value'], color='red', label="maximum", linewidth=0.5)
-plt.plot(data_range_min, data_min['Data_Value'], color='blue', label="minimum", linewidth=0.5)
 
-# Creating title
-plt.title("Maximum and minimum temperatures per day in Ann Arbor, Michigan.", fontsize=10)
-fig = plt.gcf()
-fig.set_size_inches(7, 3)
-plt.xticks(fontsize=10)
-plt.yticks(np.arange(-30, 40, 5) , fontsize=10)
 
-plt.xlabel("Years")
-plt.ylabel("°C")
-plt.legend(loc="upper right")
 
-# Filling between max and min lines
-plt.gca().fill_between(data_max.index,
-                      data_min['Data_Value'], data_max['Data_Value'],
-                      facecolor='blue', alpha=0.25)
+
+
+fig, axs = plt.subplots(2, figsize=(8, 7))
+fig.tight_layout(pad=7)
+
+
+def create_plot():
+    data_max_plot1 = data_max['2005-01-01':'2014-12-31']
+    data_min_plot1 = data_min['2005-01-01':'2014-12-31']
+
+
+    data_range_max = list(map(pd.to_datetime, data_max_plot1.index))
+    data_range_min = list(map(pd.to_datetime, data_min_plot1.index))
+
+
+    axs[0].plot(data_range_max, data_max_plot1['Data_Value'], color='red', label="maximum", linewidth=0.5)
+    axs[0].plot(data_range_min, data_min_plot1['Data_Value'], color='blue', label="minimum", linewidth=0.5)
+
+    # Creating title
+    axs[0].set_title("Maximum and minimum temperatures per day in Ann Arbor, Michigan.", fontsize=10)
+    fig = plt.gcf()
+    fig.set_size_inches(7, 4)
+    # axs[0].set_xticks()
+    axs[0].set_yticks(np.arange(-30, 40, 5))
+
+    axs[0].set_xlabel("Years", fontsize=10)
+    axs[0].set_ylabel("°C", fontsize=10)
+    axs[0].legend(loc="upper right")
+
+    # Filling between max and min lines
+    axs[0].fill_between(data_max_plot1.index,
+                        data_min_plot1['Data_Value'], data_max_plot1['Data_Value'],
+                        facecolor='blue', alpha=0.25)
+#create_plot()
+def create_plot2():
+    data_max_plot2 = data_max['2015-01-01':'2015-12-31']
+    data_min_plot2 = data_min['2015-01-01':'2015-12-31']
+    data_range_max = list(map(pd.to_datetime, data_max_plot2.index))
+    data_range_min = list(map(pd.to_datetime, data_min_plot2.index))
+
+
+    axs[1].plot(data_range_max, data_max_plot2['Data_Value'], color='red', label="maximum", linewidth=0.5)
+    axs[1].plot(data_range_min, data_min_plot2['Data_Value'], color='blue', label="minimum", linewidth=0.5)
+
+    # Creating title
+    axs[1].set_title("Maximum and minimum temperatures per day in 2015.", fontsize=10)
+    fig = plt.gcf()
+    fig.set_size_inches(7, 4)
+    axs[1].set_xticks(data_max_plot2.index[::31])
+    axs[1].set_xticklabels(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                       'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+    axs[1].set_yticks(np.arange(-30, 40, 5))
+
+    axs[1].set_xlabel("Years", fontsize=10)
+    axs[1].set_ylabel("°C", fontsize=10)
+    axs[1].legend(loc="upper right")
+
+    # Filling between max and min lines
+    axs[1].fill_between(data_max_plot2.index,
+                        data_min_plot2['Data_Value'], data_max_plot2['Data_Value'],
+                        facecolor='blue', alpha=0.25)
+create_plot()
+create_plot2()
 
 plt.show()
 
